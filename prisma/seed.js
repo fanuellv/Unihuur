86c1b583ebea073854d106e5c036f8624f5c882c
fanuellv <fanueljuniorlv2@gmail.com>
2025-04-26 07:07:26 +0100

    sexta

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,20 +1,36 @@
 const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcryptjs');
 
+if (process.env.SKIP_SEED === 'true') {
+  console.log('Skipping seed script...');
+  process.exit(0);
+}
+
 const prisma = new PrismaClient();
 
 async function main() {
-  const password = await bcrypt.hash('12345678', 10); // senha encriptada
+  const email = 'fanuell@example.com';
 
-  const user = await prisma.User.create({
+  const existingUser = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (existingUser) {
+    console.log('Usuário já existe, ignorando...');
+    return;
+  }
+
+  const password = await bcrypt.hash('12345678', 10);
+
+  const user = await prisma.user.create({
     data: {
       name: 'Fanuel',
-      email: 'fanuell@example.com',
-      password: password,
+      email,
+      password,
     },
   });
 
-  console.log('User criado:', user);
+  console.log('Usuário criado:', user);
 }
 
 main()
@@ -25,8 +41,3 @@ main()
   .finally(async () => {
     await prisma.$disconnect();
   });
-  
-  if (process.env.SKIP_SEED === 'true') {
-    console.log('Skipping seed script...');
-    process.exit(0);
-  }
