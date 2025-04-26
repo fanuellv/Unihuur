68402a94822431f1d6954829927a019054bb807a
fanuellv <fanueljuniorlv2@gmail.com>
2025-04-26 06:12:07 +0100

    sexta

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,11 +4,11 @@ import bcrypt from 'bcryptjs';
 const prisma = new PrismaClient();
 
 async function main() {
-  const password = await bcrypt.hash('123456', 10); // senha encriptada
+  const password = await bcrypt.hash('12345678', 10); // senha encriptada
 
   const user = await prisma.user.create({
     data: {
-      name: 'Fanuell',
+      name: 'Fanuel',
       email: 'fanuell@example.com',
       password: password,
     },
